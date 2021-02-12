e477a5ef4dbeebfe660485c89addb9a7d3bf8ec9
Yulia Nesterova <julianstrv@yandex.ru>
2021-02-12 11:36:43 +0300

    fix: fix per review comments

--- helpers/files.js
+++ helpers/files.js
@@ -2,11 +2,8 @@ const fsPromises = require('fs').promises;
 
 function getDataFromFile(pathToFile) {
   return fsPromises.readFile(pathToFile, { encoding: 'utf-8' })
-    .then((data) => JSON.parse(data)
-    )
-    .catch((error) => {
-      return { message: `${error.status(500)}`}
-    });
+    .then((data) => JSON.parse(data))
+    .catch((error) => ({ message: `${error.status(500)}` }));
 }
 
 module.exports = getDataFromFile;
