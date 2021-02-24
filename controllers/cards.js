397826f0533cc1682bebd672938ce7e65d8d8048
Yulia Nesterova <julianstrv@yandex.ru>
2021-02-24 22:26:47 +0300

    feat: created MondoDB and refactor

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,12 +1,69 @@
-const path = require('path');
-const getDataFromFile = require('../helpers/files');
+const Card = require('../models/card');
+const {
+  ERROR_CODE_INVALID, ERROR_CODE_NOT_FOUND, ERROR_CODE_SERVER, CODE_OK,
+} = require('../utils/responces');
 
-const dataPath = path.join(__dirname, '../', 'data', 'cards.json');
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.status(CODE_OK).send({ data: cards }))
+    .catch(() => res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' }));
+};
 
-function getCards(req, res) {
-  return getDataFromFile(dataPath)
-    .then((cards) => res.status(200).send(cards))
-    .catch(() => res.status(500).send({ message: 'Запрашиваемый ресурс не найден' }));
-}
+module.exports.createCard = (req, res) => {
+  const { name, link } = req.body;
+  const owner = req.user._id;
 
-module.exports = getCards;
+  Card.create({ name, link, owner })
+    .then((card) => res.status(CODE_OK).send({ data: card }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE_INVALID).send({ message: 'Введены невалидные данные' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.deleteCard = (req, res) => {
+  Card.findByIdAndDelete(req.params.cardId)
+    .then(() => res.status(CODE_OK).send({ message: 'Карточка удалена успешно' }))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.status(CODE_OK).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((card) => res.status(CODE_OK).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,76 @@
-const path = require('path');
-const getDataFromFile = require('../helpers/files');
+const User = require('../models/user');
+const {
+  ERROR_CODE_INVALID, ERROR_CODE_NOT_FOUND, ERROR_CODE_SERVER, CODE_OK,
+} = require('../utils/responces');
 
-const dataPath = path.join(__dirname, '../', 'data', 'users.json');
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(CODE_OK).send({ data: users }))
+    .catch(() => res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' }));
+};
 
-function getUsers(req, res) {
-  return getDataFromFile(dataPath)
-    .then((users) => res.status(200).send(users))
-    .catch(() => res.status(500).send({ message: 'Запрашиваемый ресурс не найден' }));
-}
-
-function getUserById(req, res) {
-  return getDataFromFile(dataPath)
-    .then((users) => users.find((user) => user._id === req.params.id))
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
     .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: 'Нет пользователя с таким id' });
-      }
-
-      return res.status(200).send(user);
+      res.status(CODE_OK).send(user);
     })
-    .catch(() => res.status(500).send({ message: 'Запрашиваемый ресурс не найден' }));
-}
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_CODE_NOT_FOUND).send({ message: 'Нет пользователя с таким id' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
 
-module.exports = { getUsers, getUserById };
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(CODE_OK).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE_INVALID).send({ message: 'Введены невалидные данные' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.updateUser = (req, res) => {
+  const { name, about } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { name, about }, {
+    new: true,
+    runValidators: true,
+    upsert: true,
+  })
+    .then((user) => {
+      res.status(CODE_OK).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE_INVALID).send({ message: 'Введены невалидные данные' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(req.user._id, { avatar }, {
+    new: true,
+    runValidators: true,
+    upsert: true,
+  })
+    .then((user) => res.status(CODE_OK).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE_INVALID).send({ message: 'Введены невалидные данные' });
+      } else {
+        res.status(ERROR_CODE_SERVER).send({ message: 'Произошла ошибка' });
+      }
+    });
+};
